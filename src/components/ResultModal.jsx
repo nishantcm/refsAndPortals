60cacf79ec32cd03c4d44e381bdb44f4477b2137
cmnishant <cmnishantwork@gmail.com>
2025-01-28 00:16:03 +0530

    Updated the file

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,7 +1,7 @@
-export default function ResultModal({result, targetTime}) {
+export default function ResultModal({ref, result, targetTime}) {
     return (
-        <dialog className="result-moldal" open>
-            <h2>You {result} </h2>
+        <dialog ref={ref} className="result-modal">
+            <h2>You {result}</h2>
             <p>The target time <strong>{targetTime} seconds.</strong></p>
             <p>You stopped the timer with <strong>X seconds left.</strong></p>
             <form method="dialog">
